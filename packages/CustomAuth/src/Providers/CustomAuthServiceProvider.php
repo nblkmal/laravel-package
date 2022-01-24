0e3e2d6eec6e4bf62d9e03b4e1533ae2ffa8b0a3
nblkmal <nblkmal@gmail.com>
2022-01-24 11:55:56 +0800

    add CustomAuthServiceProvider in package

---
+++ packages/CustomAuth/src/Providers/CustomAuthServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nblkmal\CustomAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomAuthServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
