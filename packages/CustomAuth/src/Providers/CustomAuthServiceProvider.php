dc04bd8c785c2c71dd68e27ca8e6f8460bbb93c0
nblkmal <nblkmal@gmail.com>
2022-01-24 12:45:16 +0800

    package : declaration for packages directory

--- packages/CustomAuth/src/Providers/CustomAuthServiceProvider.php
+++ packages/CustomAuth/src/Providers/CustomAuthServiceProvider.php
@@ -8,6 +8,7 @@ class CustomAuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        $this->loadRoutesFrom(__DIR__.'/../../routes/custom-auth.php');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'custom-auth');
     }
 }
